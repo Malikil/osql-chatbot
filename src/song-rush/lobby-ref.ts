80baf6e85d68aa298494299492faebae5fdaf65c
Eric Hagen <malikerkagen@gmail.com>
2025-08-05 00:11:13 -0700

    Remove old lines from lobby ref

--- src/song-rush/lobby-ref.ts
+++ src/song-rush/lobby-ref.ts
@@ -9,7 +9,7 @@ import {
    BanchoUser
 } from "bancho.js";
 import EventEmitter from "node:events";
-import { LobbyEvents, Mappool } from "../types/lobby";
+import { LobbyEvents } from "../types/lobby";
 import { GameMode } from "../types/global";
 
 class LobbyRef extends EventEmitter<LobbyEvents> {
@@ -32,211 +32,18 @@ class LobbyRef extends EventEmitter<LobbyEvents> {
    }
 
    async startMatch() {
-      console.log("Create lobby");
-      const lobbyChannel = await this.#bancho.createLobby(
-         `MPSQ: ${
-            this.#players[0].bancho.username
-         } (${this.#players[0].rating.rating.toFixed()}) vs ${
-            this.#players[1].bancho.username
-         } (${this.#players[1].rating.rating.toFixed()}) - ${Date.now()}`
-      );
-      this.#lobby = lobbyChannel.lobby;
-      console.log(`Created lobby: ${lobbyChannel.name}`);
-      await this.#lobby.setSettings(
-         BanchoLobbyTeamModes.HeadToHead,
-         BanchoLobbyWinConditions.ScoreV2,
-         2
-      );
-      // Set up lobby listeners
-      this.#lobby.on("playerJoined", ({ player }) => {
-         console.log(`${player.user.username} joined the lobby`);
-         if (this.#lobby.slots.filter(p => p).length > 1) {
-            // Stop waiting for players to join
-            this.#lobby.removeAllListeners("playerJoined");
-            this.#playersJoined();
-         }
-      });
-      // Invite players
-      this.#players.forEach(p => this.#lobby.invitePlayer(`#${p.bancho.id}`));
       // There must be a cleaner way to close lobbies when the program is trying to exit than
       // watching for the event here
       process.on("SIGTERM", this.#interruptHandler);
    }
 
-   async #playersJoined() {
-      console.log("Start match");
-      // Handle lobby commands
-      this.#lobby.channel.on("message", this.#handleLobbyCommand.bind(this));
-      // Handle reffing stuff
-      this.#lobby.on("allPlayersReady", this.#playersReady.bind(this));
-      this.#lobby.on("matchFinished", this.#songFinished.bind(this));
-      this.#lobby.on("playerLeft", this.#playerLeft.bind(this));
-      if (!this.#mappool) {
-         this.#lobby.channel.sendMessage("Pool failed to load, attempting refresh");
-         const playerParams = this.#players.map(p => p.bancho.id).join("&p=");
-         try {
-            await fetch(
-               `${process.env.MAPPOOL_URL}/api/db/mappool?p=${playerParams}&m=${this.#mode}`
-            )
-               .then(data => data.json())
-               .then(pool => (this.#mappool = pool as Mappool))
-               .then(() => console.log(this.#mappool));
-         } catch (err) {
-            this.#lobby.channel.sendMessage("Fetch failed, lobby will close");
-            console.warn(err);
-            setTimeout(this.closeLobby, 3000);
-            return;
-         }
-      }
-      // Send the intro message
-      const nmUrl = this.#mappool.nm.map(m => m.id).join(",");
-      const hdUrl = this.#mappool.hd.map(m => m.id).join(",");
-      const hrUrl = this.#mappool.hr.map(m => m.id).join(",");
-      const dtUrl = this.#mappool.dt.map(m => m.id).join(",");
-      const fmUrl = this.#mappool.fm.map(m => m.id).join(",");
-      const lUrl = encodeURIComponent(this.#lobby.name);
-      const searchParams = `nm=${nmUrl}&hd=${hdUrl}&hr=${hrUrl}&dt=${dtUrl}&fm=${fmUrl}&l=${lUrl}&m=${
-         this.#mode
-      }`;
-      this.#lobby.channel.sendMessage(
-         `Mappool can be found here: [${process.env.MAPPOOL_URL}/mappool/lobby?${searchParams} Mappool]`
-      );
-      this.#lobby.channel.sendMessage(
-         `First ban: ${this.#players[this.#lobbyState.nextPlayer].bancho.username}`
-      );
-   }
+   async #playersJoined() {}
 
-   /**
-    * @param {BanchoLobbyPlayer} player
-    */
-   async #playerLeft(player: BanchoLobbyPlayer) {
-      // If there are no players left, just close the lobby
-      if (this.#lobby.slots.every(p => !p)) return this.closeLobby();
-      this.#lobby.channel.sendMessage("!mp timer 150 - Player left lobby");
-      // Stop trying to start the match
-      this.#lobby.off("allPlayersReady", this.#playersReady);
-      this.#lobby.on("timerEnded", () => {
-         this.#lobby.removeAllListeners("playerJoined");
-         this.#lobby.channel.sendMessage("Match has been abandoned!");
-         this.emit("finished", this.#lobby.getHistoryUrl(), this.#lobbyState);
-         fetch(`${process.env.INTERNAL_URL}/api/db/pvp`, {
-            method: "POST",
-            body: JSON.stringify({
-               mp: this.#lobby.getHistoryUrl(),
-               playerDefault: player.user.id
-            }),
-            headers: [["Authorization", process.env.MATCH_SUBMIT_AUTH || ""]]
-         })
-            .then(
-               () => this.#lobby.channel.sendMessage("Match results submitted to server"),
-               err => console.error(err)
-            )
-            .then(() => setTimeout(this.closeLobby.bind(this), 10000));
-      });
-      this.#lobby.on("playerJoined", ({ player: joiner }) => {
-         if (player.user.id === joiner.user.id) {
-            this.#lobby.removeAllListeners("timerEnded");
-            this.#lobby.abortTimer();
-            this.#lobby.removeAllListeners("playerJoined");
-            this.#lobby.on("allPlayersReady", this.#playersReady);
-         }
-      });
-   }
+   async #playerLeft(player: BanchoLobbyPlayer) {}
 
-   /**
-    * @param {BanchoMessage} msg
-    */
-   #handleLobbyCommand(msg: BanchoMessage) {
-      if (msg.message.startsWith("!")) {
-         const command = msg.message.split(" ");
-         if (["!lobby", "!match", "!info"].includes(command[0])) {
-            const nmUrl = this.#mappool.nm.map(m => m.id).join(",");
-            const hdUrl = this.#mappool.hd.map(m => m.id).join(",");
-            const hrUrl = this.#mappool.hr.map(m => m.id).join(",");
-            const dtUrl = this.#mappool.dt.map(m => m.id).join(",");
-            const fmUrl = this.#mappool.fm.map(m => m.id).join(",");
-            const lUrl = encodeURIComponent(this.#lobby.name);
-            const searchParams = `nm=${nmUrl}&hd=${hdUrl}&hr=${hrUrl}&dt=${dtUrl}&fm=${fmUrl}&l=${lUrl}`;
-            this.#lobby.channel.sendMessage(
-               `Mappool can be found here: [${process.env.MAPPOOL_URL}/mappool/lobby?${searchParams} Mappool]`
-            );
-            this.#lobby.channel.sendMessage(
-               `${this.#players[0].bancho.username} ${this.#lobbyState.scores[0]} - ${
-                  this.#lobbyState.scores[1]
-               } ${this.#players[1].bancho.username}`
-            );
-            this.#lobby.channel.sendMessage(
-               `Next ${this.#lobbyState.action}: ${
-                  this.#players[this.#lobbyState.nextPlayer].bancho.username
-               }`
-            );
-            const unavailable = this.#lobbyState.bans.concat(this.#lobbyState.picks);
-            const maplist = ["nm", "hd", "hr", "dt", "fm"]
-               .reduce((agg, mod) => {
-                  const modMaps = this.#mappool[mod]
-                     .map((map, index) => (unavailable.includes(map) ? null : `${mod}${index + 1}`))
-                     .filter(v => v)
-                     .join(", ");
-                  agg.push(modMaps);
-                  return agg;
-               }, [])
-               .join(", ");
-            this.#lobby.channel.sendMessage(`Available maps: ${maplist}`);
-            return;
-         }
-         if (
-            command.length < 2 ||
-            msg.user.id !== this.#players[this.#lobbyState.nextPlayer].bancho.id
-         )
-            return;
-         console.log(command);
-         switch (command[0]) {
-            case "!ban":
-            case "!b":
-               if (this.#lobbyState.action === "ban") this.#banMap(command[1]);
-               else if (this.#lobbyState.action === "tbban") this.#tiebreakerBan(command[1]);
-               break;
-            case "!pick":
-            case "!p":
-               if (this.#lobbyState.action === "pick" || this.#lobbyState.action === "tb")
-                  this.#pickMap(command[1]);
-               break;
-         }
-      }
-   }
+   #handleLobbyCommand(msg: BanchoMessage) {}
 
-   async #playersReady() {
-      // Make sure the players haven't readied again after finishing a song
-      if (this.#lobbyState.picks.find(m => m.id === this.#lobby.beatmapId)) return;
-
-      if (this.#lobbyState.action === "tb") {
-         await this.#lobby.updateSettings();
-         if (
-            this.#lobby.slots.some(player => {
-               if (!player) return;
-               return !player.mods.includes(BanchoMods.NoFail);
-            })
-         )
-            return this.#lobby.channel.sendMessage("NoFail is required.");
-      } else if (this.#lobbyState.picks.selectedModpool === "fm") {
-         // Make sure both players have mods enabled
-         await this.#lobby.updateSettings();
-         if (
-            this.#lobby.slots.some(player => {
-               if (!player) return;
-               return (
-                  !player.mods.includes(BanchoMods.NoFail) ||
-                  !(
-                     player.mods.includes(BanchoMods.Hidden) ||
-                     player.mods.includes(BanchoMods.HardRock)
-                  )
-               );
-            })
-         )
-            return this.#lobby.channel.sendMessage("NoFail is required. HD or HR is required.");
-      }
-      this.#lobby.startMatch();
-   }
+   async #playersReady() {}
 
    /**
     * @param {BanchoLobbyPlayerScore[]} scores
@@ -307,4 +114,4 @@ class LobbyRef extends EventEmitter<LobbyEvents> {
    }
 }
 
-module.exports = LobbyRef;
+export default LobbyRef;
