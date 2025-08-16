f77b518c167780bb901e6609b3308d9bb9a6fb2b
Malikil <eboy.coltt5@gmail.com>
2025-08-15 17:07:01 -0700

    Fix filtering mania key count

--- src/commands/song-rush.ts
+++ src/commands/song-rush.ts
@@ -5,9 +5,9 @@ export const songRushLobby: PveCommand = async (msg, lobbyManager) => {
    // Figure out the gamemode
    let mode = msg.message.split(" ")[1] as GameMode | "ctb";
    if (mode === "ctb") mode = "fruits";
-   let maniamode = "";
+   let maniamode = null;
    if (["4k", "7k"].includes(mode)) {
-      maniamode = mode;
+      maniamode = mode as "4k" | "7k";
       mode = "mania";
    }
    if (!["osu", "fruits", "taiko", "mania"].includes(mode)) mode = "osu";
